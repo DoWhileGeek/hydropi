dcafbb4858be6bb7273c27473a5d04fb8d4efad1
Joeseph Rodrigues <dowhilegeek@gmail.com>
2020-05-13 13:17:53 -0700

    Add debug lines about fill times

--- hydropi/cycle.py
+++ hydropi/cycle.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 from time import sleep
 import logging
+import datetime
 
 import gpiozero
 from gpiozero import Button
@@ -11,18 +12,26 @@ from gpiozero import Button
 from hydropi.devices import low_float, high_float, in_pump, out_pump
 from hydropi.config import config
 from hydropi.utils.logging import setup_logging
+from hydropi.utils.pid_lock import pid_lock
 
 
 def fill(args):
     logging.info("turning on in_pump")
+    pre_fill = datetime.datetime.utcnow()
     in_pump.on()
 
     high_float.wait_for_release()
+    logging.debug(
+        f"seconds from empty to filled: {(datetime.datetime.utcnow() - pre_fill).total_seconds()}"
+    )
 
     logging.info(f"overfilling for {config['overfill']} seconds")
     sleep(config["overfill"])
 
     in_pump.off()
+    logging.debug(
+        f"seconds from emmpty to overfilled: {(datetime.datetime.utcnow() - pre_fill).total_seconds()}"
+    )
 
 
 def drain(args):
@@ -56,37 +65,38 @@ def ping(args):
 def main():
     setup_logging()
 
-    try:
-        # create the top-level parser
-        parser = argparse.ArgumentParser()
-        subparsers = parser.add_subparsers()
+    with pid_lock():
+        try:
+            # create the top-level parser
+            parser = argparse.ArgumentParser()
+            subparsers = parser.add_subparsers()
 
-        # create the parser for the "fill" command
-        parser_fill = subparsers.add_parser("fill")
-        parser_fill.set_defaults(func=fill)
+            # create the parser for the "fill" command
+            parser_fill = subparsers.add_parser("fill")
+            parser_fill.set_defaults(func=fill)
 
-        # create the parser for the "drain" command
-        parser_drain = subparsers.add_parser("drain")
-        parser_drain.set_defaults(func=drain)
+            # create the parser for the "drain" command
+            parser_drain = subparsers.add_parser("drain")
+            parser_drain.set_defaults(func=drain)
 
-        # create the parser for the "cycle" command
-        parser_drain = subparsers.add_parser("cycle")
-        parser_drain.set_defaults(func=cycle)
+            # create the parser for the "cycle" command
+            parser_drain = subparsers.add_parser("cycle")
+            parser_drain.set_defaults(func=cycle)
 
-        # create the parser for the "drain:tank" command
-        parser_drain = subparsers.add_parser("drain:tank")
-        parser_drain.set_defaults(func=drain_tank)
+            # create the parser for the "drain:tank" command
+            parser_drain = subparsers.add_parser("drain:tank")
+            parser_drain.set_defaults(func=drain_tank)
 
-        # create the parser for the "ping" command
-        parser_drain = subparsers.add_parser("ping")
-        parser_drain.set_defaults(func=ping)
+            # create the parser for the "ping" command
+            parser_drain = subparsers.add_parser("ping")
+            parser_drain.set_defaults(func=ping)
 
-        # parse the args and call whatever function was selected
-        args = parser.parse_args()
-        args.func(args)
+            # parse the args and call whatever function was selected
+            args = parser.parse_args()
+            args.func(args)
 
-    except KeyboardInterrupt:
-        logging.info("cancelling")
+        except KeyboardInterrupt:
+            logging.info("cancelling")
 
 
 # utilities
--- hydropi/utils/logging.py
+++ hydropi/utils/logging.py
@@ -5,6 +5,6 @@ def setup_logging():
     logging.basicConfig(
         format="%(asctime)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%dT%H:%M:%S%z",
-        filename="~/hydropi.log",
+        filename="/home/pi/hydropi.log",
         level=logging.DEBUG,
     )
