1cb284238f1ac3ffa42754eb0e72076a4901d59e
Joeseph Rodrigues <dowhilegeek@gmail.com>
2020-05-13 12:47:00 -0700

    Fix log setup fn

--- hydropi/utils/logging.py
+++ hydropi/utils/logging.py
@@ -1,6 +1,10 @@
 import logging
 
+
 def setup_logging():
     logging.basicConfig(
-        format="%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%dT%H:%M:%S%z", level=logging.DEBUG)
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S%z",
+        filename="~/hydropi.log",
+        level=logging.DEBUG,
     )
